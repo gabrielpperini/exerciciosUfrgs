66c7ad4886ec057d6d192dfceebec2864e89231f
Gabriel Perini <gabrielprestesperini@hotmail.com>
2021-08-06 15:04:57 -0300

    - add 01 exercises

---
+++ gabriel-01-a.c
@@ -0,0 +1,29 @@
+/*
+    UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL
+    INF01202 - ALGORITMOS E PROGRAMAÇÃO
+
+    ATIVIDADE PRÁTICA
+    SEMANA 01 / EXERCÍCIO 01-b
+
+    ALUNO: Gabriel Prestes Perini
+    MATRÍCULA: 00334582
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char nome[256];
+    printf("Digite seu nome: ");
+    scanf("%[0-9a-zA-Z ]",nome);
+    int tamanho = strlen(nome);
+    char line[100] = "";
+    for (int i = 0; i < (tamanho + 2); i++)
+    {
+        strcat(line,"=");
+    }
+    
+    printf("/%s\\\n| %s |\n\\%s/", line, nome, line);
+    return 0;
+}
---
+++ gabriel-01-b.c
@@ -0,0 +1,35 @@
+/*
+    UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL
+    INF01202 - ALGORITMOS E PROGRAMAÇÃO
+
+    ATIVIDADE PRÁTICA
+    SEMANA 01 / EXERCÍCIO 01-a
+
+    ALUNO: Gabriel Prestes Perini
+    MATRÍCULA: 00334582
+    
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("                           =====   \n");
+    printf("     ______________________!!!!!___\n");
+    printf("    /                      !!!!!   \\\n");
+    printf("   /                       =====    \\\n");
+    printf("  /                                  \\\n");
+    printf(" /                                    \\\n");
+    printf("/______________________________________\\\n");
+    printf("   ||     _____          _____     ||\n");
+    printf("   ||     | | |          | | |     ||\n");
+    printf("   ||     |-|-|          |-|-|     ||\n");
+    printf("   ||     #####          #####     ||\n");
+    printf("   ||                              ||\n");
+    printf("   ||     _____   ----   _____     ||\n");
+    printf("   ||     | | |   @@@@   | | |     ||\n");
+    printf("   ||     |-|-|   @@@@   |-|-|     ||\n");
+    printf("   ||     #####   @@@@   #####     ||\n");
+    printf("   ||             @@@@             ||\n");
+    printf("******************____*****************\n");
+    return 0;
+}
